5cf5ddbd746db49004457eeeb91ca059ce65fe98
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-02-10 05:00:30 +0000

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,9 +8,23 @@ class RecipesController < ApplicationController
   end
 
   def new
+    
     @recipe = Recipe.new
+    2.times { @recipe.ingredients.build }
   end
 
   def create
-  end
+     #binding.pry
+     @recipe = Recipe.create(recipe_params)
+     redirect_to recipe_path(@recipe)
+     
+     end
+     
+   private 
+   
+   def recipe_params
+       params.require(:recipe).permit(:title,ingredients_attributes: [:name,:quantity])
+       
+   end
+   
 end
